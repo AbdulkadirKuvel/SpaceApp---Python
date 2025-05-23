5b17c8860778f758dd53ee459e06d54fb26fd06a
AbdulkadirKuvel <abdulkadir.kuvel@ogr.sakarya.edu.tr>
2025-05-23 07:52:00 +0300

    Added functions from simulator

--- src/utils/FileReading.py
+++ src/utils/FileReading.py
@@ -2,20 +2,106 @@
 from src.entities.Planets.Planet import Planet
 from src.entities.Spacecraft import Spacecraft
 from src.entities.Person import Person
+import fontstyle as fs # type: ignore
+from readchar import readkey, key  # type: ignore
 from typing import List
+import sys
 import os
+
+def clear_lines(n = 1):
+    for _ in range(n):
+        sys.stdout.write("\033[F")
+        sys.stdout.write("\033[K")
 
 class FileReading():
     def __init__(self, people_file_name: str,
                  spaceship_file_name: str,
                  planets_file_name: str):
         self.people_path = people_file_name
-        self.spaceship_path = spaceship_file_name
+        self.spacecraft_path = spaceship_file_name
         self.planet_path = planets_file_name
 
         self.spacecrafts: List[Spacecraft] = []
         self.planets: List[Planet] = [] # TODO
         self.people: List[Person] = []
+
+
+    def path_changer_menu(self):
+        ax1 = fs.apply("path >>", "green")
+        ax2 = fs.apply("       ", "green")
+        ax3 = fs.apply("       ", "green")
+        ax4 = fs.apply("       ", "green")
+
+        line = 1
+
+        print("Current paths:")
+        print(f"People path: {self.people_path}")
+        print(f"Spacecraft path: {self.spacecraft_path}")
+        print(f"Planet path: {self.planet_path}")
+
+        while True:
+            print(f"{ax1} Change People path")
+            print(f"{ax2} Change Spacecraft path")
+            print(f"{ax3} Change Planet path")
+            print(f"{ax4} Back")
+            _key = readkey()
+            if _key == key.UP:
+                line -= 1
+                if line < 1:
+                    line = 4
+            elif _key == key.DOWN:
+                line += 1
+                if line > 4:
+                    line = 1
+            elif _key == key.ENTER:
+                if line == 1:
+                    self.people_path_changer()
+                elif line == 2:
+                    self.spacecraft_path_changer()
+                elif line == 3:
+                    self.planet_path_changer()
+                elif line == 4:
+                    return
+            else:
+                pass
+            clear_lines(4)
+
+            if line == 1:
+                ax1 = fs.apply("path >>", "green")
+                ax2 = fs.apply("       ", "")
+                ax3 = fs.apply("       ", "")
+                ax4 = fs.apply("       ", "")
+                
+            elif line == 2:
+                ax1 = fs.apply("       ", "")
+                ax2 = fs.apply("path >>", "green")
+                ax3 = fs.apply("       ", "")
+                ax4 = fs.apply("       ", "")
+
+            elif line == 3:
+                ax1 = fs.apply("       ", "")
+                ax2 = fs.apply("       ", "")
+                ax3 = fs.apply("path >>", "green")
+                ax4 = fs.apply("       ", "")
+
+            elif line == 4:
+                ax1 = fs.apply("       ", "")
+                ax2 = fs.apply("       ", "")
+                ax3 = fs.apply("       ", "")
+                ax4 = fs.apply("path >>", "green")
+
+    def people_path_changer(self):
+        self.people_path = input(fs.apply("New path for people file: ", "yellow"))
+        print("New path:", self.people_path)
+
+    def spacecraft_path_changer(self):
+        self.spacecraft_path = input(fs.apply("New path for spacecraft file: ", "yellow"))
+        print("New path:", self.spacecraft_path)
+
+    def planet_path_changer(self):
+        self.planets_path = input(fs.apply("New path for planet file: ", "yellow"))
+        print("New path:", self.planets_path)
+
 
     def control_people(self) -> bool:
         if os.path.exists(self.people_path):
@@ -24,7 +110,7 @@ class FileReading():
             return False
 
     def control_spacecrafts(self) -> bool:
-        if os.path.exists(self.spaceship_path):
+        if os.path.exists(self.spacecraft_path):
             return True
         else:
             return False
@@ -58,7 +144,7 @@ class FileReading():
     #     return self.planets
 
     def readSpaceships(self):
-        with open(self.spaceship_path, encoding="utf-8") as file:
+        with open(self.spacecraft_path, encoding="utf-8") as file:
             for line in file:
                 line = line.rstrip()
                 parts = line.split("#")
